930d4ebe23ed0fdb0dd710d83bf4c1008ac4e1c1
hejiang <1615387927@qq.com>
2023-02-22 18:51:45 +0800

    02.22

--- app/src/main/java/com/cohelp/task_for_stu/ui/activity/user/CreateNewTaskActivity.java
+++ app/src/main/java/com/cohelp/task_for_stu/ui/activity/user/CreateNewTaskActivity.java
@@ -1,24 +1,21 @@
 package com.cohelp.task_for_stu.ui.activity.user;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.net.Uri;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
+import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,30 +25,23 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bigkoo.pickerview.TimePickerView;
 import com.cohelp.task_for_stu.R;
-import com.cohelp.task_for_stu.UserInfoHolder;
 import com.cohelp.task_for_stu.bean.BaseTask;
-import com.cohelp.task_for_stu.bean.User;
 import com.cohelp.task_for_stu.biz.TaskBiz;
 import com.cohelp.task_for_stu.config.Config;
-import com.cohelp.task_for_stu.net.CommonCallback;
-import com.cohelp.task_for_stu.net.OKHttpTools.OKHttp;
 import com.cohelp.task_for_stu.net.OKHttpTools.OkHttpUtils;
 import com.cohelp.task_for_stu.net.model.entity.Activity;
 import com.cohelp.task_for_stu.ui.activity.BaseActivity;
-import com.cohelp.task_for_stu.ui.adpter.ImageSelectGridAdapter;
+import com.cohelp.task_for_stu.ui.adpter.FullyGridLayoutManager;
+import com.cohelp.task_for_stu.ui.adpter.GridImageAdapter;
 import com.cohelp.task_for_stu.ui.vo.Task;
-import com.cohelp.task_for_stu.utils.BasicUtils;
-import com.cohelp.task_for_stu.utils.ImageTool;
-import com.cohelp.task_for_stu.utils.PictureUtils;
 import com.cohelp.task_for_stu.utils.SessionUtils;
-import com.cohelp.task_for_stu.utils.T;
 import com.luck.picture.lib.PictureSelector;
 import com.luck.picture.lib.config.PictureConfig;
+import com.luck.picture.lib.config.PictureMimeType;
 import com.luck.picture.lib.entity.LocalMedia;
-import com.xuexiang.xui.widget.shadow.ShadowButton;
+import com.luck.picture.lib.permissions.Permission;
+import com.luck.picture.lib.permissions.RxPermissions;
 
-import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -59,10 +49,11 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
+import io.reactivex.functions.Consumer;
 
 @RequiresApi(api = Build.VERSION_CODES.O)
-public class CreateNewTaskActivity extends BaseActivity implements ImageSelectGridAdapter.OnAddPicClickListener {
+public class CreateNewTaskActivity extends BaseActivity  {
     EditText title;
     EditText content;
     EditText reward;
@@ -76,11 +67,14 @@ public class CreateNewTaskActivity extends BaseActivity implements ImageSelectGr
     TimePickerView pickerView;
     OkHttpUtils okHttpUtils = new OkHttpUtils();
 
-    RecyclerView recyclerView;
 
-    private ImageSelectGridAdapter mAdapter;
+    private int maxSelectNum = 9;
+    private List<LocalMedia> selectList = new ArrayList<>();
+    private GridImageAdapter adapter;
+    private RecyclerView mRecyclerView;
+    private PopupWindow pop;
 
-    private List<LocalMedia> mSelectList = new ArrayList<>();
+
 
     private static final int IMG_COUNT = 8;
     private static final String IMG_ADD_TAG = "a";
@@ -95,11 +89,15 @@ public class CreateNewTaskActivity extends BaseActivity implements ImageSelectGr
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_task);
         okHttpUtils.setCookie(SessionUtils.getCookiePreference(this));
-//        askPermissions();
+        askPermissions();
+
         setUpToolBar();
         setTitle("创建活动");
         initView();
         initEvent();
+//        takePhoto();
+        initWidget();
+//        takePhoto();
     }
     private void initEvent() {
 //        endTime.setOnClickListener(new View.OnClickListener() {
@@ -153,7 +151,7 @@ public class CreateNewTaskActivity extends BaseActivity implements ImageSelectGr
         startTime = findViewById(R.id.id_et_startDate);
 //        endTime = findViewById(R.id.id_et_endDate);
         gridView = findViewById(R.id.gridview);
-        recyclerView = findViewById(R.id.id_recycler_view);
+        mRecyclerView = findViewById(R.id.id_recycler_view);
         task = new Task();
         taskBiz = new TaskBiz();
         baseTask = new BaseTask();
@@ -185,45 +183,235 @@ public class CreateNewTaskActivity extends BaseActivity implements ImageSelectGr
                 .setDecorView(null)
                 .build();
 
-        GridLayoutManager manager = new GridLayoutManager(this, 4, RecyclerView.VERTICAL, false);
-        recyclerView.setLayoutManager(manager);
-        recyclerView.setAdapter(mAdapter = new ImageSelectGridAdapter(this, this));
-        mAdapter.setSelectList(mSelectList);
-        mAdapter.setSelectMax(8);
-        mAdapter.setOnItemClickListener((position, v) -> PictureSelector.create(CreateNewTaskActivity.this).themeStyle(R.style.XUIPictureStyle).openExternalPreview(position, mSelectList));
+//        GridLayoutManager manager = new GridLayoutManager(this, 4, RecyclerView.VERTICAL, false);
+//        recyclerView.setLayoutManager(manager);
+//        recyclerView.setAdapter(mAdapter = new ImageSelectGridAdapter(this, this));
+//        mAdapter.setSelectList(mSelectList);
+//        mAdapter.setSelectMax(8);
+//        mAdapter.setOnItemClickListener((position, v) -> PictureSelector.create(CreateNewTaskActivity.this).themeStyle(R.style.XUIPictureStyle).openExternalPreview(position, mSelectList));
 
     }
 
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == android.app.Activity.RESULT_OK) {
-            switch (requestCode) {
-                case PictureConfig.CHOOSE_REQUEST:
-                    // 图片选择
-                    mSelectList = PictureSelector.obtainMultipleResult(data);
-                    mAdapter.setSelectList(mSelectList);
-                    mAdapter.notifyDataSetChanged();
-                    break;
-                default:
-                    break;
+    private void initWidget() {
+        FullyGridLayoutManager manager = new FullyGridLayoutManager(this, 3, GridLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(manager);
+        adapter = new GridImageAdapter(this, onAddPicClickListener);
+        adapter.setList(selectList);
+        adapter.setSelectMax(maxSelectNum);
+        mRecyclerView.setAdapter(adapter);
+        adapter.setOnItemClickListener(new GridImageAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position, View v) {
+                if (selectList.size() > 0) {
+                    LocalMedia media = selectList.get(position);
+                    String pictureType = media.getPictureType();
+                    int mediaType = PictureMimeType.pictureToVideo(pictureType);
+                    switch (mediaType) {
+                        case 1:
+                            // 预览图片 可自定长按保存路径
+//                            PictureSelector.create(CreateNewTaskActivity.this).externalPicturePreview(position, "/custom_file", selectList);
+                            PictureSelector.create(CreateNewTaskActivity.this).externalPicturePreview(position, selectList);
+                            break;
+                        case 2:
+                            // 预览视频
+                            PictureSelector.create(CreateNewTaskActivity.this).externalPictureVideo(media.getPath());
+                            break;
+                        case 3:
+                            // 预览音频
+                            PictureSelector.create(CreateNewTaskActivity.this).externalPictureAudio(media.getPath());
+                            break;
+                    }
+                }
             }
+        });
+    }
+
+    private GridImageAdapter.onAddPicClickListener onAddPicClickListener = new GridImageAdapter.onAddPicClickListener() {
+
+        @SuppressLint("CheckResult")
+        @Override
+        public void onAddPicClick() {
+            //获取写的权限
+            RxPermissions rxPermission = new RxPermissions(CreateNewTaskActivity.this);
+            rxPermission.requestEach(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    .subscribe(new Consumer<Permission>() {
+                        @Override
+                        public void accept(Permission permission) {
+                            if (permission.granted) {// 用户已经同意该权限
+                                //第一种方式，弹出选择和拍照的dialog
+                                showPop();
+
+                                //第二种方式，直接进入相册，但是 是有拍照得按钮的
+//                                showAlbum();
+                            } else {
+                                Toast.makeText(CreateNewTaskActivity.this, "拒绝", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    };
+
+    private void showAlbum() {
+        //参数很多，根据需要添加
+        PictureSelector.create(CreateNewTaskActivity.this)
+                .openGallery(PictureMimeType.ofImage())// 全部.PictureMimeType.ofAll()、图片.ofImage()、视频.ofVideo()、音频.ofAudio()
+                .maxSelectNum(maxSelectNum)// 最大图片选择数量
+                .minSelectNum(1)// 最小选择数量
+                .imageSpanCount(4)// 每行显示个数
+                .selectionMode(PictureConfig.MULTIPLE)// 多选 or 单选PictureConfig.MULTIPLE : PictureConfig.SINGLE
+                .previewImage(true)// 是否可预览图片
+                .isCamera(true)// 是否显示拍照按钮
+                .isZoomAnim(true)// 图片列表点击 缩放效果 默认true
+                //.setOutputCameraPath("/CustomPath")// 自定义拍照保存路径
+                .enableCrop(true)// 是否裁剪
+                .compress(true)// 是否压缩
+                //.sizeMultiplier(0.5f)// glide 加载图片大小 0~1之间 如设置 .glideOverride()无效
+                .glideOverride(160, 160)// glide 加载宽高，越小图片列表越流畅，但会影响列表图片浏览的清晰度
+                .withAspectRatio(1, 1)// 裁剪比例 如16:9 3:2 3:4 1:1 可自定义
+                //.selectionMedia(selectList)// 是否传入已选图片
+                //.previewEggs(false)// 预览图片时 是否增强左右滑动图片体验(图片滑动一半即可看到上一张是否选中)
+                //.cropCompressQuality(90)// 裁剪压缩质量 默认100
+                //.compressMaxKB()//压缩最大值kb compressGrade()为Luban.CUSTOM_GEAR有效
+                //.compressWH() // 压缩宽高比 compressGrade()为Luban.CUSTOM_GEAR有效
+                //.cropWH()// 裁剪宽高比，设置如果大于图片本身宽高则无效
+                .rotateEnabled(false) // 裁剪是否可旋转图片
+                //.scaleEnabled()// 裁剪是否可放大缩小图片
+                //.recordVideoSecond()//录制视频秒数 默认60s
+                .forResult(PictureConfig.CHOOSE_REQUEST);//结果回调onActivityResult code
+    }
+
+    private void showPop() {
+        View bottomView = View.inflate(CreateNewTaskActivity.this, R.layout.layout_bottom_dialog, null);
+        TextView mAlbum = bottomView.findViewById(R.id.tv_album);
+//        TextView mCamera = bottomView.findViewById(R.id.tv_camera);
+        TextView mCancel = bottomView.findViewById(R.id.tv_cancel);
+
+        pop = new PopupWindow(bottomView, -1, -2);
+        pop.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        pop.setOutsideTouchable(true);
+        pop.setFocusable(true);
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        lp.alpha = 0.5f;
+        getWindow().setAttributes(lp);
+        pop.setOnDismissListener(new PopupWindow.OnDismissListener() {
+
+            @Override
+            public void onDismiss() {
+                WindowManager.LayoutParams lp = getWindow().getAttributes();
+                lp.alpha = 1f;
+                getWindow().setAttributes(lp);
+            }
+        });
+        pop.setAnimationStyle(R.style.picker_view_slide_anim);
+        pop.showAtLocation(getWindow().getDecorView(), Gravity.BOTTOM, 0, 0);
+
+        View.OnClickListener clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.tv_album:
+                        //相册
+                        PictureSelector.create(CreateNewTaskActivity.this)
+                                .openGallery(PictureMimeType.ofImage())
+                                .maxSelectNum(maxSelectNum)
+                                .minSelectNum(1)
+                                .imageSpanCount(4)
+                                .selectionMode(PictureConfig.MULTIPLE)
+                                .forResult(PictureConfig.CHOOSE_REQUEST);
+                        break;
+//                    case R.id.tv_camera:
+//                        //拍照
+//                        System.out.println("22222");
+//                        PictureSelector.create(CreateNewTaskActivity.this)
+//                                .openCamera(PictureMimeType.ofImage())
+//                                .forResult(PictureConfig.CHOOSE_REQUEST);
+//                        break;
+                    case R.id.tv_cancel:
+                        //取消
+                        //closePopupWindow();
+                        break;
+                }
+                closePopupWindow();
+            }
+        };
+
+        mAlbum.setOnClickListener(clickListener);
+//        mCamera.setOnClickListener(clickListener);
+        mCancel.setOnClickListener(clickListener);
+    }
+
+    public void closePopupWindow() {
+        if (pop != null && pop.isShowing()) {
+            pop.dismiss();
+            pop = null;
         }
     }
 
-    @Override
-    public void onAddPicClick() {
-        PictureUtils.getPictureSelector(this)
-                .selectionMedia(mSelectList)
-                .forResult(PictureConfig.CHOOSE_REQUEST);
-    }
 
 
     @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-        super.onPointerCaptureChanged(hasCapture);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        List<LocalMedia> images;
+        System.out.println("rc"+resultCode+requestCode);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == PictureConfig.CHOOSE_REQUEST) {// 图片选择结果回调
+                System.out.println("p1111");
+                System.out.println(data);
+//                final Uri uri = data.getData();
+//                String path = ImageTool.getImageAbsolutePath(this,uri);
+//                System.out.println(path);
+                images = PictureSelector.obtainMultipleResult(data);
+                System.out.println(images);
+                selectList.addAll(images);
+                System.out.println(selectList);
+                //selectList = PictureSelector.obtainMultipleResult(data);
+
+                // 例如 LocalMedia 里面返回三种path
+                // 1.media.getPath(); 为原图path
+                // 2.media.getCutPath();为裁剪后path，需判断media.isCut();是否为true
+                // 3.media.getCompressPath();为压缩后path，需判断media.isCompressed();是否为true
+                // 如果裁剪并压缩了，以取压缩路径为准，因为是先裁剪后压缩的
+                adapter.setList(selectList);
+                adapter.notifyDataSetChanged();
+            }
+        }
+
     }
+
+
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (resultCode == android.app.Activity.RESULT_OK) {
+//            switch (requestCode) {
+//                case PictureConfig.CHOOSE_REQUEST:
+//                    // 图片选择
+//                    mSelectList = PictureSelector.obtainMultipleResult(data);
+//                    mAdapter.setSelectList(mSelectList);
+//                    mAdapter.notifyDataSetChanged();
+//                    break;
+//                default:
+//                    break;
+//            }
+//        }
+//    }
+
+//    @Override
+//    public void onAddPicClick() {
+//        PictureUtils.getPictureSelector(this)
+//                .selectionMedia(mSelectList)
+//                .forResult(PictureConfig.CHOOSE_REQUEST);
+//    }
+
+
+//    @Override
+//    public void onPointerCaptureChanged(boolean hasCapture) {
+//        super.onPointerCaptureChanged(hasCapture);
+//    }
 
 
     private void toTaskCenterActivity() {
@@ -369,21 +557,35 @@ public class CreateNewTaskActivity extends BaseActivity implements ImageSelectGr
 //
 //    }
 //
-//    private void askPermissions() {//动态申请权限！
-//        if (Build.VERSION.SDK_INT >= 23) {
-//            int REQUEST_CODE_CONTACT = 101;
-//            String[] permissions = {Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_CONTACTS,//联系人的权限
-//                    Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};//读写SD卡权限
-//            //验证是否许可权限
-//            for (String str : permissions) {
-//                if (this.checkSelfPermission(str) != PackageManager.PERMISSION_GRANTED) {
-//                    //申请权限
-//                    this.requestPermissions(permissions, REQUEST_CODE_CONTACT);
-//                }
-//            }
+    private void askPermissions() {//动态申请权限！
+        if (Build.VERSION.SDK_INT >= 23) {
+            int REQUEST_CODE_CONTACT = 101;
+            String[] permissions = {Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_CONTACTS,//联系人的权限
+                    Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};//读写SD卡权限
+            //验证是否许可权限
+            for (String str : permissions) {
+                if (this.checkSelfPermission(str) != PackageManager.PERMISSION_GRANTED) {
+                    //申请权限
+                    this.requestPermissions(permissions, REQUEST_CODE_CONTACT);
+                }
+            }
+        }
+
+    }
+//    @NeedsPermission({Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE})
+//    protected void takePhoto() {
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//            String filename = new SimpleDateFormat("yyyyMMdd-HHmmss", Locale.CHINA).format(new Date()) + ".png";
+//            File file = new File(Environment.getExternalStorageDirectory(), filename);
+//            String mCurrentPhotoPath = file.getAbsolutePath();
+//            // 仅需改变这一行
+//            Uri fileUri = FileProvider7.getUriForFile(this, file);
+//            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+//            startActivityForResult(takePictureIntent, PictureConfig.CAMERA);
 //        }
-//
 //    }
+
 
 
 
