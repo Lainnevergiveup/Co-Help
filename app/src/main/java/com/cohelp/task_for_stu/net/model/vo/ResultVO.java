dd8d843709a4d253b6c974377f4bbcd7f69fe49d
lain <2836969767@qq.com>
2023-03-27 18:40:39 +0800

    2023.03.27

---
+++ app/src/main/java/com/cohelp/task_for_stu/net/model/vo/ResultVO.java
@@ -0,0 +1,46 @@
+package com.cohelp.task_for_stu.net.model.vo;
+
+import com.cohelp.task_for_stu.net.model.domain.DetailResponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author zgy
+ * @create 2023-03-20 18:40
+ */
+public class ResultVO {
+    /**
+     * 收藏记录/浏览记录id
+     */
+    private Integer id;
+    /**
+     * 详情
+     */
+    private DetailResponse detailResponse;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public DetailResponse getDetailResponse() {
+        return detailResponse;
+    }
+
+    public void setDetailResponse(DetailResponse detailResponse) {
+        this.detailResponse = detailResponse;
+    }
+
+    public ResultVO(Integer id, DetailResponse detailResponse) {
+        this.id = id;
+        this.detailResponse = detailResponse;
+    }
+
+    public ResultVO() {
+    }
+}
